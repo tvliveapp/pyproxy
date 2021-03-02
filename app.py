0286798efa3b6cc9e74affc8342071078e404db0
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2021-03-02 16:42:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def iptvhd(id):
     url = "http://iptvhd.club/aptv/vip/cablehd.php?id="
     headers = CaseInsensitiveDict()
     headers["User-Agent"] = "loaB6oIZOla892u1q9qoi9j31BrzLRzQgfTrvXYGnfIVrkwb9402QA44LkyZf98xmMznUTqpoIZoeQk"
+    headers["Referer"] = ""
     resp = requests.get(url, headers=headers)
     a=resp.text.split('Clappr.Player(')[1]
     b=a.split('{')[1]
