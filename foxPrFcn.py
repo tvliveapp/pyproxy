7d84db09e35d5760b7fd3ff11e1f59d136ccc690
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2021-03-18 23:12:07 -0500

    Add files via upload

---
+++ foxPrFcn.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import base64
+#import aes
+from requests.structures import CaseInsensitiveDict
+#"fs1ar"  "fs2ar" "fs3ar" "fspar"
+url1="https://cdn.tsnt.xyz/"
+url2=".json"
+
+def foxPrFcn(url):
+    print('fox',url)
+    headers = CaseInsensitiveDict()
+    resp = requests.get(url1+url+url2)
+    print(resp.text)
+    urlStr=json.loads(resp.text)
+    print(urlStr["source"])
+    base64_bytes = urlStr["source"].encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+
+    print(message)
+    return message
+def tntSp():
+    url='https://sv.televisionlibre.net/json/tntsports.json'
+    print('tnt',url)
+    headers = CaseInsensitiveDict()
+    resp = requests.get(url)
+    print(resp.text)
+    urlStr=json.loads(resp.text)
+    print(urlStr["token"])
+    base64_bytes = urlStr["source"].encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+
+    print(message)
+    return message
+foxPrFcn("fspar")
+tntSp()
